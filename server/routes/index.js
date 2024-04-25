ba0f6c81ed335d4e74c9547375a63b678beaa079
Alain Mena Galindo <alain@Alains-MacBook-Pro.local>
2024-04-25 00:36:39 -0400

    [1] Up.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,53 @@
 
-const router = require('express').Router();
+console.log('***** SERVER:', Date.now());
 
-router.use('/api/obs', require('./api/obs'));
-router.use('/api/litra', require('./api/litra'));
-router.use('/api/screenshot', require('./api/screenshot'));
+const express = require('express');
+const app = express();
 
-module.exports = router;
+const api = {
+	host: process.env.SERVER_HOST || '127.0.0.1',
+	port: process.env.SERVER_PORT || 8715,
+};
+
+api.url = `http://${ api.host }:${ api.port }`;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/obs', require('./api/obs'));
+app.use('/api/litra', require('./api/litra'));
+app.use('/api/screenshot', require('./api/screenshot'));
+
+app.getRoutes = () =>
+{
+	let routes = [];
+
+	for (route of app._router.stack)
+	{
+		if (!route.handle.stack) continue;
+	
+		let regex = route.regexp.toString();
+		let path = decodeURIComponent(regex).match(/^\/(.+?)\/?(?=\(|$)/).pop().replace(/^\^/, '').replace(/\\/g, '').replace('/?', '');
+	
+		for (layer of route.handle.stack) routes.push({
+			url: api.url + path + (layer?.route?.path || ''),
+			path: path + (layer?.route?.path || ''),
+			methods: Object.keys(layer?.route?.methods || {}).join(',').toUpperCase().split(',')
+		})
+	}
+
+	return routes;
+};
+
+app.listen(api.port, api.host, () =>
+{
+	console.log('***** HTTP:', api.url);
+
+	for ({ url, path, methods } of app.getRoutes())
+	{
+		console.log('***** HTTP:', url, methods);
+	}
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,4 @@
 
 require('dotenv').config({ path: __dirname + '/../.env' });
 
-const litra = require('litra');
-const express = require('express');
-const app = express();
 const routes = require('./routes');
-
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(routes);
-
-app.listen(8715, () => {
-	console.log('*****', 'http://localhost:8715/')
-});
